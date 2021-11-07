c55520ae6943ac7c13ffe0f7912b988b1b76c76e
jhoncone <40128739+jhoncone@users.noreply.github.com>
2021-11-07 00:16:51 -0500

    Update PokeService.js

--- src/components/service/PokeService.js
+++ src/components/service/PokeService.js
@@ -1,19 +1,15 @@
 import axios from 'axios';
 const POKE_API_BASE_URL="http://localhost:8080/api/poke";
-//const POKE_API_BASE_URL="http://localhost:8080/";
-//const PRODUCTO_API_BASE_URLS="http://localhost:8080/producto/list/";
 class PokeService{
 
 
   getPokesAll(){
-   //  return axios.get(POKE_API_BASE_URL+'listar/');
    return axios.get(POKE_API_BASE_URL+ "/all");
   }
 
 
 
   getPokeById(pokeId){
-  //  return axios.get(POKE_API_BASE_URL+'/listar/'+ pokeId)
   return axios.get(POKE_API_BASE_URL+'/' + pokeId + "/info");
   }
 
